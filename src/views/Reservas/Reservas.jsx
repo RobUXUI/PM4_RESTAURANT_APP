e0adee3c0196ba3f294e3b36339c7980c53b434c
RobUXUI <roberto.munizaga@bootcampudd.cl>
2023-11-08 22:18:31 -0300

    Actualización formulario db firestore

--- src/views/Reservas/Reservas.jsx
+++ src/views/Reservas/Reservas.jsx
@@ -1,250 +1,69 @@
-// import { useState } from 'react';
-// import { Button, Col, Form, Row, Container } from 'react-bootstrap';
-// import '../Reservas/Reservas.css';
-// import ListaReservas from '../../componentes/ListaReservas';
-
-
-
-// function Reservas() {
-//   // Initialize state for each form field
-//   const [nombre, setNombre] = useState('');
-//   const [email, setEmail] = useState('');
-//   const [telefono, setTelefono] = useState('');
-//   const [ciudad, setCiudad] = useState('');
-//   const [mensaje, setMensaje] = useState('');
-//   const [comensales, setComensales] = useState('');
-//   const [terminos, setTerminos] = useState(false);
-  
-//   // State to manage form validation
-//   const [validated, setValidated] = useState(false);
-  
-//   // State to check if the reservation was successful
-//   const [reservaRealizada, setReservaRealizada] = useState(false);
-
-//   // Form submission handler
-//   const handleSubmit = (event) => {
-//     const form = event.currentTarget;
-//     event.preventDefault();
-//     event.stopPropagation();
-//     if (form.checkValidity()) {
-     
-//       console.log('Form data', { nombre,email, telefono, ciudad, mensaje, comensales });
- 
-//       setReservaRealizada(true);
-//     }
-//     setValidated(true);
-//   };
-
-//   if (reservaRealizada) {
-//     return (
-//       <Container className="my-3 px-3">
-//         <div>
-//         <h2>Reserva realizada</h2>
-//         <p>¡Tu reserva ha sido confirmada con éxito!</p>
-//         </div>       
-//       </Container>
-//     );
-//   }
-
-//   // Form rendering
-//   return (
-
-//      <Container className="my-3 px-3">
-//      <Row className="m-5"> 
-//      <Col md={6} className="datos_contacto mb-2">
-//      <h2>Reserva con nosotros  </h2>
-//      <h6>A partir de tu contacto, nos comunicaremos contigo <br /> para revisar fechas y disponibilidad de horarios. </h6>
-//     <br />
-//         <Container  fluid>
-//         <h6> telefono: +1 (555) 123-4567</h6>
-//         <h6> contacto@bestchoice.com</h6>
-//         </Container>
-//      </Col>
-//      <Col md={6} className="formulario_reserva mb-2">
-//       <h2>Formulario de Reservas</h2>
-//       <Form noValidate validated={validated} onSubmit={handleSubmit}>
-//         <Row className="mb-3">
-//           {/* Nombre input */}
-//           <Form.Group as={Col} controlId="validationCustom01">
-//             <Form.Label>Nombre Completo</Form.Label>
-//             <Form.Control
-//               required
-//               type="text"
-//               placeholder="Nombre"
-//               value={nombre}
-//               onChange={(e) => setNombre(e.target.value)}
-//             />
-//             <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-//           </Form.Group>
-          
-        
-//         </Row>
-//         <Row className="mb-3">
-
-//         {/* Email input */}
-//         <Form.Group as={Col} md="4" controlId="validationCustom03">
-//           <Form.Label>Email</Form.Label>
-//           <Form.Control
-//             type="email"
-//             placeholder="Email"
-//             required
-//             value={email}
-//             onChange={(e) => setEmail(e.target.value)}
-//           />
-//           <Form.Control.Feedback type="invalid">
-//             Please provide a valid email.
-//           </Form.Control.Feedback>
-//         </Form.Group>
-
-//         {/* Teléfono input */}
-//         <Form.Group as={Col} md="4" controlId="validationCustom04">
-//           <Form.Label>Teléfono</Form.Label>
-//           <Form.Control
-//             type="tel"
-//             placeholder="Teléfono"
-//             required
-//             value={telefono}
-//             onChange={(e) => setTelefono(e.target.value)}
-//           />
-//           <Form.Control.Feedback type="invalid">
-//             Proporcionar un número correcto
-//           </Form.Control.Feedback>
-//         </Form.Group>
-
-//          {/* Ciudad input */}
-//          <Form.Group as={Col} md="4" controlId="validationCustom05">
-//           <Form.Label>Ciudad</Form.Label>
-//           <Form.Control
-//             type="text"
-//             placeholder="Ciudad"
-//             required
-//             value={ciudad}
-//             onChange={(e) => setCiudad(e.target.value)}
-//           />
-//           <Form.Control.Feedback type="invalid">
-//             Please provide a valid city.
-//           </Form.Control.Feedback>
-//         </Form.Group>
-//         </Row>
-      
-//         <Row className="mb-3">
-//         {/* Mensaje textarea */}
-//         <Form.Group as={Col} controlId="validationCustom06">
-//           <Form.Label>Mensaje</Form.Label>
-//           <Form.Control
-//             as="textarea"
-//             placeholder="Mensaje"
-//             required
-//             value={mensaje}
-//             onChange={(e) => setMensaje(e.target.value)}
-//           />
-//           <Form.Control.Feedback type="invalid">
-//             Please provide a message.
-//           </Form.Control.Feedback>
-//         </Form.Group>
-//         </Row>
-//         <Row className="mb-3">
-//         {/* Cantidad de Comensales select */}
-//         <Form.Group as={Col} controlId="validationCustomComensales">
-//           <Form.Label>Cantidad de Comensales</Form.Label>
-//           <Form.Control
-//             as="select"
-//             required
-//             value={comensales}
-//             onChange={(e) => setComensales(e.target.value)}
-//           >
-//             <option value="">Seleccione...</option>
-//             {[...Array(10)].map((_, index) => (
-//               <option key={index} value={index + 1}>
-//                 {index + 1}
-//               </option>
-//             ))}
-//           </Form.Control>
-//           <Form.Control.Feedback type="invalid">
-//             Please select the number of diners.
-//           </Form.Control.Feedback>
-//         </Form.Group>
-//         </Row>
-//         {/* Términos y condiciones checkbox */}
-//         <Form.Group className="mb-3">
-//           <Form.Check
-//             required
-//             label="Acepto los términos y condiciones impartidos por el local"
-//             feedback="Debe aceptar los términos y condiciones antes de enviar."
-//             feedbackType="invalid"
-//             checked={terminos}
-//             onChange={(e) => setTerminos(e.target.checked)}
-//           />
-//         </Form.Group>
-
-//         {/* Submit button */}
-//         <Button variant="danger" type="submit">Enviar</Button>
-//       </Form>
-//       </Col>
-//       </Row>
-
-//       <Row>
-//       <ListaReservas/>
-//       </Row>
-//     </Container>
-//   );
-// }
-
-// export default Reservas;
-
 
 import { useState } from 'react';
 import { Button, Col, Form, Row, Container } from 'react-bootstrap';
 import '../Reservas/Reservas.css';
+import { db } from "../../config/Firebase";
+import { collection, addDoc } from 'firebase/firestore';
 import ListaReservas from '../../componentes/ListaReservas';
 
 function Reservas() {
-  // Inicializar estado para cada campo del formulario
+  // Estados para cada campo del formulario
   const [nombre, setNombre] = useState('');
   const [email, setEmail] = useState('');
+  const [fechaHora, setFechaHora] = useState('');
   const [telefono, setTelefono] = useState('');
   const [ciudad, setCiudad] = useState('');
   const [mensaje, setMensaje] = useState('');
   const [comensales, setComensales] = useState('');
   const [terminos, setTerminos] = useState(false);
   
-  // Estado para gestionar la validación del formulario
+  // Estado para la validación del formulario
   const [validated, setValidated] = useState(false);
   
-  // Estado para comprobar si la reserva se realizó con éxito
+  // Estado para la confirmación de la reserva
   const [reservaRealizada, setReservaRealizada] = useState(false);
 
-  // Manejador para el envío del formulario
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    event.preventDefault();
-    event.stopPropagation();
-    if (form.checkValidity()) {
-      // Registra los datos del formulario en la consola
-      console.log('Datos del formulario', { nombre, email, telefono, ciudad, mensaje, comensales });
-      // Marca la reserva como realizada
-      setReservaRealizada(true);
+  // Función para guardar la reserva en Firebase
+  const guardarReserva = async (datosReserva) => {
+    try {
+      await addDoc(collection(db, "Reservas"), datosReserva);
+      setReservaRealizada(true); // Marcar la reserva como realizada
+      console.log('Datos del formulario', { nombre, email, telefono, ciudad, mensaje, comensales,fechaHora });
+    } catch (error) {
+      console.error("Error al guardar la reserva: ", error);
     }
-    // Activa la validación del formulario
-    setValidated(true);
   };
 
-  // Si se realizó la reserva, mostrar mensaje de confirmación
+  // Manejador del envío del formulario
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    } else {
+      // Crear objeto con los datos del formulario
+      const datosReserva = { nombre, email, fechaHora, telefono, ciudad, mensaje, comensales };
+      guardarReserva(datosReserva); // Llamar a la función para guardar los datos
+    }
+    setValidated(true); // Activar la validación de Bootstrap
+  };
+
+  // Si la reserva se realizó con éxito, mostrar mensaje de confirmación
   if (reservaRealizada) {
     return (
       <Container className="my-3 px-3">
-        <div>
-          <h2>Reserva Realizada</h2>
-          <p>¡Tu reserva ha sido confirmada con éxito!</p>
-        </div>       
+        <h2>Reserva Realizada</h2>
+        <p>¡Tu reserva ha sido confirmada con éxito!</p>
+        <ListaReservas />
       </Container>
     );
   }
 
-  // Renderizar formulario
+  // Renderizar formulario de reserva
   return (
     <Container className="my-3 px-3">
       <Row className="m-5">
+        {/* Información de contacto */}
         <Col md={6} className="datos_contacto mb-2">
           <h2>Reserva con nosotros</h2>
           <h6>Tras tu contacto, nos pondremos en comunicación contigo para confirmar fechas y disponibilidad de horarios.</h6>
@@ -254,6 +73,7 @@ function Reservas() {
             <h6>contacto@bestchoice.com</h6>
           </Container>
         </Col>
+        {/* Formulario de reserva */}
         <Col md={6} className="formulario_reserva mb-2">
           <h2>Formulario de Reservas</h2>
           <Form noValidate validated={validated} onSubmit={handleSubmit}>
@@ -317,56 +137,63 @@ function Reservas() {
                 </Form.Control.Feedback>
               </Form.Group>
             </Row>
-
             <Row className="mb-3">
-              {/* Selector de número de comensales */}
+              {/* Entrada para el número de comensales */}
               <Form.Group as={Col} controlId="validationCustom02">
-                <Form.Label>Número de Comensales</Form.Label>
+                <Form.Label>Comensales</Form.Label>
                 <Form.Control
                   required
                   type="number"
-                  placeholder="Comensales"
+                  placeholder="Número de comensales"
                   value={comensales}
                   onChange={(e) => setComensales(e.target.value)}
                 />
-                <Form.Control.Feedback>¡Perfecto!</Form.Control.Feedback>
+                <Form.Control.Feedback>¡Todo correcto!</Form.Control.Feedback>
+              </Form.Group>
+
+              {/* Entrada para fecha y hora */}
+              <Form.Group as={Col} controlId="validationCustomFechaHora">
+                <Form.Label>Fecha y hora</Form.Label>
+                <Form.Control
+                  required
+                  type="datetime-local"
+                  placeholder="Fecha y hora"
+                  value={fechaHora}
+                  onChange={(e) => setFechaHora(e.target.value)}
+                />
+                <Form.Control.Feedback>¡Todo correcto!</Form.Control.Feedback>
               </Form.Group>
             </Row>
-
             <Row className="mb-3">
               {/* Área de texto para el mensaje */}
-              <Form.Group as={Col} controlId="validationCustom03">
+              <Form.Group as={Col} controlId="validationCustom06">
                 <Form.Label>Mensaje</Form.Label>
                 <Form.Control
                   as="textarea"
-                  placeholder="Deja aquí tu mensaje"
-                  required
+                  placeholder="Deja tu mensaje aquí..."
+                  style={{ height: '100px' }}
                   value={mensaje}
                   onChange={(e) => setMensaje(e.target.value)}
                 />
-                <Form.Control.Feedback type="invalid">
-                  Por favor, introduce un mensaje.
-                </Form.Control.Feedback>
               </Form.Group>
             </Row>
-
-            <Form.Group className="mb-3">
-              <Form.Check
-                required
-                label="Acepto los términos y condiciones"
-                feedback="Debes aceptar los términos y condiciones antes de enviar."
-                feedbackType="invalid"
-                checked={terminos}
-                onChange={(e) => setTerminos(e.target.checked)}
-              />
-            </Form.Group>
-            <Button variant='danger' type="submit">Enviar</Button>
+            <Row className="mb-3">
+              {/* Checkbox para los términos y condiciones */}
+              <Form.Group as={Col}>
+                <Form.Check
+                  required
+                  label="Acepto términos y condiciones"
+                  feedback="Debes estar de acuerdo antes de enviar."
+                  feedbackType="invalid"
+                  checked={terminos}
+                  onChange={(e) => setTerminos(e.target.checked)}
+                />
+              </Form.Group>
+            </Row>
+            <Button type="submit">Enviar</Button>
           </Form>
         </Col>
       </Row>
-      <Row>
-   <ListaReservas/>
-       </Row>
     </Container>
   );
 }
