dd6f4936cbace57ddbe16d5abdb80fcf526411e7
RobUXUI <roberto.munizaga@bootcampudd.cl>
2023-11-14 18:21:14 -0300

    netlify

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  // base: "/PM4_RESTAURANT_APP",
-  plugins: [react()],
-  build: {
+
+ 
+
+  export default defineConfig({
+    // base: "/PM4_RESTAURANT_APP/",
+    plugins: [react()],
+    build: {
     outDir: 'dist', 
   },
 })
